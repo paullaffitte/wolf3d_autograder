8f69b28dfcd86e6c901d1a351cb567bbf1cfbf98
Paul Laffitte <paul.laffitte@epitech.eu>
2016-12-21 18:44:27 +0100

    Fix autograder, libgraph not found

--- autograder.sh
+++ autograder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm diffs -rf
 mkdir diffs
-gcc tests/autograder.c tests/test-put_pixel.c tests/test-draw_line.c tests/test-move_forward.c tests/test-raycast.c tests/free_raycast.c src/*.c -W -Wall -Werror -lc_graph_prog -lm -lgraph -o autograder -g
+gcc tests/autograder.c tests/test-put_pixel.c tests/test-draw_line.c tests/test-move_forward.c tests/test-raycast.c tests/free_raycast.c src/*.c -W -Wall -Werror -L./ -lc_graph_prog -lm -lgraph -o autograder -g
 valgrind ./autograder
 rm autograder
