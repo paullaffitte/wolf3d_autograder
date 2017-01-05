8a91b35313538a0232b6e80e42edc281127f070f
Paul Laffitte <paul.laffitte@epitech.eu>
2017-01-05 10:35:39 +0100

    Update compilation

--- autograder.sh
+++ autograder.sh
@@ -24,6 +24,6 @@
 
 rm diffs -rf
 mkdir diffs
-gcc tests/autograder.c tests/test-put_pixel.c tests/test-draw_line.c tests/test-move_forward.c tests/test-raycast.c tests/free_raycast.c src/*.c -W -Wall -Werror -I./include -I./inc -I./ -L./ -lc_graph_prog -lm -lgraph -o autograder -g
+gcc tests/autograder.c tests/test-put_pixel.c tests/test-draw_line.c tests/test-move_forward.c tests/test-raycast.c tests/free_raycast.c src/my_put_pixel.c src/my_draw_line.c src/move_forward.c src/raycast.c src/raycast_part2.c -W -Wall -Werror -I./include -I./inc -I./ -L./ -lc_graph_prog -lm -lgraph -o autograder -g
 valgrind ./autograder
 rm autograder
